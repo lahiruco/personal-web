47c5de985abecfa7ac60398316831ee355e1cefc
Lahiru Shehan Kumarasinghe <161362008+lahiruco@users.noreply.github.com>
2026-08-07 09:31:59 +0530

    Image test

--- src/data/products.ts
+++ src/data/products.ts
@@ -10,7 +10,7 @@ export const products: Product[] = [
     price: 0.00,
     currency: 'USD',
     type: 'digital',
-    imageUrl: 'https://z-p3-scontent.fcmb9-1.fna.fbcdn.net/v/t39.30808-6/762332095_10165801552385961_5831547648527528652_n.jpg?stp=dst-jpg_tt6&cstp=mx2048x1366&ctp=p960x960&_nc_cat=104&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeE53rR0QjX7OuieM7blU8CBUrpCZe6ZYaVSukJl7plhpWhCJYwbIrYVGRidhxSqCRlxzp1e16uMcQb58nC3okZJ&_nc_ohc=riQbdlJbM14Q7kNvwFwaz5F&_nc_oc=Adrm94lo54fDrzHVmdXfTC6SbzxlQWqoDOJV3qzG3U1jJKQwlU_3qKGJV0Gy3S8P9MM&_nc_zt=23&_nc_ht=z-p3-scontent.fcmb9-1.fna&_nc_gid=TT-jKqSHE4an2qFnJe1YmQ&_nc_ss=7b2a8&oh=00_AQFhAgfTTTzg6K3oOYV87TOrsDtzTjKe3XjbudHcTopz2Q&oe=6A7B148A',
+    imageUrl: 'https://postimg.cc/8fJDxNLM',
     buyUrl: 'https://muddy-minnow-fed.notion.site/Automotive-Mastery-System-cd7ef94598204f718e0e881586b61798?pvs=143',
     featured: true,
     badge: 'New',
